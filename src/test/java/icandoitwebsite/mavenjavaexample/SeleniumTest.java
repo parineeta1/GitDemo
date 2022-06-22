f4256be64101b8ac3bf30024c67d89962c1d1b33
Pari <paritutor@gmail.com>
2022-06-21 21:51:35 -0400

    first commit

---
+++ src/test/java/icandoitwebsite/mavenjavaexample/SeleniumTest.java
@@ -0,0 +1,16 @@
+package icandoitwebsite.mavenjavaexample;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void browserUIAuotmation() {
+		System.out.println("browserUIAuotmation");
+	}
+
+	@Test
+	public void loginUIAuto() {
+		System.out.println("loginUIAuto");
+	}
+}
